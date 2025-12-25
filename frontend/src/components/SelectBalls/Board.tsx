5599bb1bc34e95feb79408fa9367a396983afd9c
keshavDooleea <kdooleea@yahoo.ca>
2025-12-25 02:31:50 -0500

    wip

--- frontend/src/components/SelectBalls/Board.tsx
+++ frontend/src/components/SelectBalls/Board.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, MouseEvent } from 'react';
 
 interface IBoard {
   color: string;
@@ -9,15 +9,29 @@ interface IBoard {
 export function Board({ color, name, onClick }: IBoard) {
   const [isClicked, setIsClicked] = useState(false);
 
+  const handleClick = (e: MouseEvent<HTMLDivElement>) => {
+    setIsClicked(true);
+
+    setTimeout(() => {
+      setIsClicked(false);
+
+      if (onClick) {
+        setTimeout(() => onClick(), 150);
+      }
+    }, 200);
+  };
+
   return (
     <div
-      className={`who-breaks-board board ${isClicked ? 'clicked' : ''}`}
+      className={`board ${isClicked ? 'clicked' : ''}`}
       style={{ backgroundColor: color }}
-      onClick={onClick}>
+      onClick={handleClick}>
       <div className='board-name' contentEditable>
         {name}
       </div>
-      <div className='board-score board-ball'>s</div>
+      <div className='board-score board-ball'>
+        <img src={`balls/${name.toLowerCase()}.png`} alt={`${name} ball`} />
+      </div>
     </div>
   );
 }
