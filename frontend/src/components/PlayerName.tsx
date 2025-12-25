a358003ae9d7b51ce643b3b4648c03d12b89c17d
keshavDooleea <kdooleea@yahoo.ca>
2025-12-25 10:57:16 -0500

    wip

--- frontend/src/components/PlayerName.tsx
+++ frontend/src/components/PlayerName.tsx
@@ -15,6 +15,7 @@ export function PlayerName({ name, onNameChanged }: IPlayerNameProps) {
       contentEditable
       onInput={handleInput}
       suppressContentEditableWarning={true}
+      spellCheck={false}
     />
   );
 }
