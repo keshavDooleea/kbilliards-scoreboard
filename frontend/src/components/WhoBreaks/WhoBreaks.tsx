b28d12dbfb137f46fbc820637bc4d93995e154dc
keshavDooleea <kdooleea@yahoo.ca>
2025-12-25 01:25:18 -0500

    wip

--- frontend/src/components/WhoBreaks/WhoBreaks.tsx
+++ frontend/src/components/WhoBreaks/WhoBreaks.tsx
@@ -1,5 +1,6 @@
 import { useBoardColor, usePlayerName } from '../../context';
 import { Board } from './Board';
+import './WhoBreaks.css';
 
 export function WhoBreaks() {
   const { lColor, rColor } = useBoardColor();
@@ -7,6 +8,10 @@ export function WhoBreaks() {
 
   return (
     <div className='board-container'>
+      <div id='title-container'>
+        <h2>Who breaks?</h2>
+      </div>
+
       <Board color={lColor} name={lName} />
       <div className='separator'></div>
       <Board color={rColor} name={rName} />
